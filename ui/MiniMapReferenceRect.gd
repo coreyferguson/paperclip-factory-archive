76869ae67dc7b224d69ba165811d8a1eee3d5aa4
Corey Ferguson <corey.t.ferguson@gmail.com>
2018-09-23 12:57:37 -0700

    minimap mouse pressed within bounds

--- ui/MiniMapReferenceRect.gd
+++ ui/MiniMapReferenceRect.gd
@@ -2,8 +2,20 @@ extends ReferenceRect
 
 signal mouse_button_pressed(local_position)
 
+var mouse_pressed = false
+var mouse_pressed_in_bounds = false
+
 func _process(delta):
 	if Input.is_mouse_button_pressed(BUTTON_LEFT):
-		var pos = get_local_mouse_position()
-		if pos.x >= 0 and pos.y >= 0 and pos.x <= rect_size.x and pos.y <= rect_size.y:
-			emit_signal('mouse_button_pressed', pos)
+		var position = get_local_mouse_position()
+		if !mouse_pressed and is_in_bounds(position):
+			mouse_pressed = true
+			mouse_pressed_in_bounds = true
+		if mouse_pressed_in_bounds:emit_signal('mouse_button_pressed', position)
+		mouse_pressed = true
+	else:
+		mouse_pressed = false
+		mouse_pressed_in_bounds = false
+
+func is_in_bounds(position):
+	return position.x >= 0 and position.y >= 0 and position.x <= rect_size.x and position.y <= rect_size.y
