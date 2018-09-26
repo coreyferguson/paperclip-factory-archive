da0da48b375c35f99021f73e059293a7824d6204
Corey Ferguson <corey.t.ferguson@gmail.com>
2018-09-25 17:12:05 -0700

    paperclip factory must be next to moon

--- ui/BuildPaperClipFactory.gd
+++ ui/BuildPaperClipFactory.gd
@@ -1,4 +1,9 @@
 extends Area2D
 
 func is_valid_position():
-	return true
+	var overlapping = get_overlapping_bodies()
+	if overlapping.size() > 0:
+		for node in overlapping:
+			if node.has_method('can_harvest_type') and node.can_harvest_type('iron'):
+				return true
+	return false
