a108389ca1f686127ace56527829ec860d3768aa
Corey Ferguson <corey.t.ferguson@gmail.com>
2018-10-06 15:53:08 -0700

    Refactored build menu hover with custom panel

--- ui/BuildItemHover.gd
+++ ui/BuildItemHover.gd
@@ -1,4 +1,4 @@
-extends PanelContainer
+extends MarginContainer
 
 var build_item_hover_requirement_resource = load('res://ui/BuildItemHoverRequirement.tscn')
 
@@ -6,7 +6,8 @@ onready var container = $'MarginContainer/VBoxContainer/RequirementsContainer'
 onready var shortcut = $'MarginContainer/VBoxContainer/spacer/HBoxContainer/Shortcut'
 
 func _process(delta):
-	rect_position = get_global_mouse_position() + Vector2(50, -25)
+	rect_position = get_global_mouse_position() + Vector2(50, rect_size.y/-2)
+	rect_position.y = clamp(rect_position.y, 0, OS.get_real_window_size().y-rect_size.y-50)
 
 func _on_BuildMenu_hover_in(build_item_type):
 	var build_item = Build.Items[build_item_type]
@@ -26,6 +27,7 @@ func _on_BuildMenu_hover_out(build_item):
 	visible = false
 	for child in container.get_children():
 		container.remove_child(child)
+#	rect_size.x = 0
 	rect_size.y = 0
 
 func get_required_resources(build_item):
