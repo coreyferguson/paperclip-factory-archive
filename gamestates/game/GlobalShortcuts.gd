d401331850d0002260f3c797f0f60a275974f17c
Corey Ferguson <corey.t.ferguson@gmail.com>
2018-10-19 07:39:21 -0700

    Pause game

--- gamestates/game/GlobalShortcuts.gd
+++ gamestates/game/GlobalShortcuts.gd
@@ -1,5 +1,7 @@
 extends Node
 
+onready var game = $'/root/Game'
+
 func _unhandled_input(event):
 	handleKeyEvents(event)
 
@@ -7,3 +9,5 @@ func handleKeyEvents(event):
 	if event is InputEventKey and event.pressed:
 		if event.scancode == KEY_F11:
 			OS.window_fullscreen = !OS.window_fullscreen
+		if event.scancode == KEY_PAUSE:
+			get_tree().paused = !get_tree().paused
